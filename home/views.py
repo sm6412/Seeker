a7ff7412185958c50d03b4a11a201e135444eb3f
Samira <sm6412@nyu.edu>
2019-11-28 18:12:40 -0500

    made a sample home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+codes = [
+    {
+        'device_name' : "Samira's iPhone",
+        'qr_code' : "1234"
+    },
+    {
+        'device_name' : "Samira's calculator",
+        'qr_code' : "1235"
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Seeker Home</h1>')
+    context = {
+        'qr_codes':codes
+    }
+    return render(request,'home/home.html',context)
